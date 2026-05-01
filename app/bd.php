d7da87bdad0f3a29871a5b94b3b178de54d3e864
admin <admin@ngibr.onmicrosoft.com>
2026-05-01 10:48:39 -0300

    Ajuste de conexão com BD Postress

--- app/bd.php
+++ app/bd.php
@@ -2,9 +2,16 @@
 
 //Conexão com o banco de dados
 
-$dbServer = getenv("DB_SERVER") ? getenv("DB_SERVER") : '127.0.0.1';
-$dbUser = getenv("DB_USER") ? getenv("DB_USER") : 'root';
-$dbPassword = getenv("DB_PASSWORD") ? getenv("DB_PASSWORD") : '';
-$dbName = getenv("DB_NAME") ? getenv("DB_NAME") : 'bdcontta';
+$dbServer = getenv("DB_SERVER") ?: '127.0.0.1';
+$dbUser = getenv("DB_USER") ?: 'sa';
+$dbUser = getenv("DB_USER") ?: 'postgres';
+$dbPassword = getenv("DB_PASSWORD") ?: '1234';
+$dbName = getenv("DB_NAME") ?: 'bdcontta';
 
-$bdConexao = mysqli_connect($dbServer, $dbUser, $dbPassword, $dbName);
+$connectionString = "host=$dbServer dbname=$dbName user=$dbUser password=$dbPassword";
+$bdConexao = pg_connect($connectionString);
+
+if (!$bdConexao) {
+    // Em produção, você pode querer logar o erro em vez de exibi-lo com die()
+    die("Erro na conexão com o banco de dados: " . pg_last_error());
+}
